3fbea10528847410193a351c84267fa12b7304ce
Amin. P <33692052+pouramin@users.noreply.github.com>
2026-09-21 20:35:34 +0200

    Select bundled engine inside Kilo adapter

--- cmd/launcher/runtime_kilo.go
+++ cmd/launcher/runtime_kilo.go
@@ -16,6 +16,10 @@ import (
 
 type kiloRuntimeEngine struct{}
 
+func defaultRuntimeEngine() runtimeEngine {
+	return kiloRuntimeEngine{}
+}
+
 func (kiloRuntimeEngine) ID() string {
 	return "kilo-code"
 }
